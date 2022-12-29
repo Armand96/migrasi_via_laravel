2601cd01a987be06b229f210e37958be4809e93a
Armand Iskandarsyah <armand.iskandar96@gmail.com>
2022-12-29 12:46:13 +0700

    post oracle

--- app/Console/Commands/PostToOracle.php
+++ app/Console/Commands/PostToOracle.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 class PostToOracle extends Command
 {
@@ -41,7 +43,7 @@ class PostToOracle extends Command
         // dd(date('m/d/Y-H:i:s'));
 
         /* AMBIL DATA DARI TRANSAKSI SUMMARY */
-        $sqlBatch = "SELECT batch FROM acc_sm_trans WHERE isPost = 0 LIMIT 10";
+        $sqlBatch = "SELECT batch, idSmTrans FROM acc_sm_trans WHERE isPost = 0 LIMIT 10";
         $dataBatch = DB::connection('mysql')->select(DB::raw($sqlBatch));
         $stringDataBatch = "";
 
@@ -52,6 +54,7 @@ class PostToOracle extends Command
         $stringDataBatch = substr($stringDataBatch, 0, -1);
 
         $sqlDetail = "SELECT
+            idDtTrans,
             kodeTransaksi AS TrxNumber,
             namaJenisJurnal AS TrxType,
             DATE_FORMAT(dt.tanggal, '%d-%b-%y') AS TrxDate,
@@ -68,12 +71,14 @@ class PostToOracle extends Command
             END AS Company,
             LEFT(coaCabang, 3) AS Outlet,
             1 AS TipePembayaran,
-            '000' AS CostCenter,
-            ct.coaOracle AS NaturalAccount,
             CASE
-                WHEN LEFT(kodeTransaksi, 1) = 0 THEN (SELECT productOracleValue FROM trans_gadai tg LEFT JOIN tblproduk tp ON tp.idProduk = tg.idProduk WHERE noSbg = kodeTransaksi LIMIT 1)
-                ELSE ''
-            END AS Product,
+                WHEN SUBSTR(kodeTransaksi, 6, 3) = 'PTC' THEN rpc.kodeCostCenter
+                WHEN SUBSTR(kodeTransaksi, 6, 3) = 'ADV' THEN adv.kodeCostCenter
+                WHEN SUBSTR(kodeTransaksi, 6, 3) = 'RKN' THEN rb.kodeCostCenter
+                ELSE '000'
+            END AS CostCenter,
+            ct.coaOracle AS NaturalAccount,
+            CASE WHEN tbp.productOracleValue IS NULL THEN '' ELSE productOracleValue END AS Product,
             CASE
                 WHEN idUsedfor != 0 THEN '0000'
                 ELSE '0000'
@@ -93,18 +98,96 @@ class PostToOracle extends Command
             '' AS BankReference
         FROM `acc_dt_trans` dt
         LEFT JOIN acc_sm_trans sm ON sm.batch = dt.batch
+        LEFT JOIN tblproduk tbp ON tbp.idProduk = sm.idProduk
         LEFT JOIN acc_jenisjurnal jj ON jj.idJenisJurnal = sm.idJenisJurnal
         LEFT JOIN tblcoatemplate ct ON ct.idCoa = dt.idCoa
+        LEFT JOIN (
+            SELECT kodeVoucher, CASE WHEN kodeCostCenter IS NULL THEN '000' ELSE kodeCostCenter END AS kodeCostCenter
+            FROM advance adv
+            LEFT JOIN advance_realisasi_detail adt ON adt.idAdvance = adv.idAdvance
+            LEFT JOIN tblcostcenter tcc ON tcc.idCostCenter = adt.idCostCenter
+        ) AS adv ON adv.kodeVoucher = kodeTransaksi
+        LEFT JOIN (
+            SELECT noVoucher, CASE WHEN kodeCostCenter IS NULL THEN '000' ELSE kodeCostCenter END AS kodeCostCenter
+            FROM fa_rekonbank rb
+            LEFT JOIN fa_rekonbank_detail rbd ON rb.idRekonBank = rbd.idRekonBank
+            LEFT JOIN tblcostcenter tcc ON tcc.idCostCenter = rbd.idCostCenter
+        ) AS rb ON rb.noVoucher = kodeTransaksi
+        LEFT JOIN (
+            SELECT kodeVoucher, CASE WHEN kodeCostCenter IS NULL THEN '000' ELSE kodeCostCenter END AS kodeCostCenter
+            FROM realisasi_pettycash rpc
+            LEFT JOIN realisasi_pettycash_detail rpd ON rpc.idRealisasiPettyCash = rpd.idRealisasiPettyCash
+            LEFT JOIN tblcostcenter tcc ON tcc.idCostCenter = rpd.idCostCenter
+        ) AS rpc ON rpc.kodeVoucher = kodeTransaksi
         -- LEFT JOIN tblcoausedfor cuf ON cuf.idUsedfor = ct.idUsedfor
         WHERE dt.batch IN ($stringDataBatch);";
 
         $dataBatchDetail = DB::connection('mysql')->select(DB::raw($sqlDetail));
+
+        DB::beginTransaction();
+
+        foreach ($dataBatch as $bIndex => $bVal) {
+            $updateData = array(
+                'isPost' => 1
+            );
+            DB::connection('mysql')->table('acc_sm_trans')->where('idSmTrans', $bVal->idSmTrans)->update($updateData);
+            $rowIndex = 1;
+            foreach ($dataBatchDetail as $key => $value) {
+                if($value->batch == $bVal->batch) {
+                    $dataBatchDetail[$key]->LineID = intval($value->batch . $rowIndex);
+                    $rowIndex++;
+                }
+            }
+        }
+
+        // Storage::disk('public')->put('batch.json', json_encode($dataBatchDetail));
+        // dd($dataBatchDetail);
+
+        $headers = array(
+            'Authorization' => 'Basic RUJTQ01BU0RFVjpDMzUzRDg1MDQwNzA5NDJENjNBRUJCOUFFRkM4QUVFNQ==',
+            'Content-Type' => 'text/plain'
+        );
 
         $dataPost = array(
             'BatchName' => date('m/d/Y-H:i:s'),
             'CountLine' => count($dataBatchDetail),
             'Journal' => $dataBatchDetail
         );
+
+        $response = Http::withHeaders($headers)->post('https://api.serbamuliagroup.co.id/ebs/dev/mas/core', $dataPost);
+        $bodyResponse = json_decode($response->body());
+
+        $batchOracleInsert = array(
+            'tanggalJam' => date('Y-m-d H:i:s'),
+            'isStatus' => 0,
+            'sent' => json_encode($dataPost),
+            'response' => $response->body(),
+        );
+
+        /* SET RESPONSE */
+        if($bodyResponse->status == 'success') $batchOracleInsert['isStatus'] = 1;
+        else $batchOracleInsert['isStatus'] = 2;
+
+        /* INSERT TO DB BATCH ORACLE */
+        $dataBatch = DB::connection('mysql')->table('acc_batch_oracle')->insertGetId($batchOracleInsert);
+        $lastInsertBatchID = DB::connection('mysql')->table('acc_batch_oracle')->select('idBatch')->orderBy('idBatch', 'desc')->first();
+
+        // dd($lastInsertBatchID);
+
+        /* UPDATE ACC_DT_TRANS */
+        foreach ($dataBatchDetail as $key => $value) {
+            $dataUpdate = array(
+                'lineId' => $value->LineID,
+                'statusOracle' => $batchOracleInsert['isStatus'],
+                'idBatch' => $lastInsertBatchID->idBatch
+            );
+            DB::connection('mysql')->table('acc_dt_trans')->where('idDtTrans', $value->idDtTrans)->update($dataUpdate);
+            // unset($dataBatchDetail[$key]->batch);
+        }
+
+        DB::commit();
+
+        dd($dataBatch, $lastInsertBatchID);
 
         return 0;
     }
